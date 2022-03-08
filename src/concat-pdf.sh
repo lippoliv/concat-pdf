69493ee1668336d34106a541bac0aebf74302b56
Oliver Lippert <oliver@allesit.de>
2022-03-08 22:19:55 +0100

    call pdfunite and pass in the arguments
    first argument should be the output file
    
    Signed-off-by: Oliver Lippert <oliver@allesit.de>

--- src/concat-pdf.sh
+++ src/concat-pdf.sh
@@ -10,4 +10,13 @@ failIfCommandNotFound() {
    exit 1
 }
 
+concatPdf() {
+  target=$1
+  shift
+
+  # shellcheck disable=SC2068
+  pdfunite $@ "$target"
+}
+
 failIfCommandNotFound pdfunite
+concatPdf "$@"
